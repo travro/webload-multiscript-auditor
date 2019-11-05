7f20a9971b6620ce60a942f9597c8ce7b9bced04
tsmelvin <travis.melvin@sumtotalsystems.com>
2019-11-05 14:58:21 -0500

    UPDATE MainWindow.cs
    added base functionallity to main window

--- WMSA_Project/MainWindow.xaml.cs
+++ WMSA_Project/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WMSA_Project.Controls;
+using WMSA_Project.Models.Repositories;
 
 namespace WMSA_Project
 {
@@ -23,6 +26,28 @@ namespace WMSA_Project
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (var scc in ScriptRepository.Repository.SCCList)
+            {
+                StkPnl_Main.Children.Add(scc);
+            }
+
+            if (ScriptRepository.Repository != null)
+            {
+                ScriptRepository.Repository.CollectionChanged += UpdateList;
+            }
         }
+
+        #region handlers
+        private void UpdateList(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            StkPnl_Main.Children.Clear();
+
+            foreach (var scc in ScriptRepository.Repository.SCCList)
+            {
+                StkPnl_Main.Children.Add(scc);
+            }
+        }
+        #endregion
     }
 }
