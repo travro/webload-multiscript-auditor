264744cef95500bf012e3c9fd24a495841a6e979
tsmelvin <travis.melvin@sumtotalsystems.com>
2019-11-08 17:04:01 -0500

    UPDATE Script.cs
    added TestName property that is settable

--- WMSA_Project/Models/Script.cs
+++ WMSA_Project/Models/Script.cs
@@ -9,18 +9,23 @@ namespace WMSA_Project.Models
     public class Script
     {
         public int Id { get; }
+        public string TestName { get; set; }
+        public string BuildVersion { get; set; }
         public string Name { get; set; }
         public DateTime RecordedDate { get; set; }
-        public int TestId { get; }
+
         public List<Transaction> Transactions { get; set; }
 
         public Script()
         {
             Transactions = new List<Transaction>();
         }
-        public Script(string name)
+        public Script(string testName, string buildVers, string name, DateTime dateTime)
         {
+            TestName = testName;
+            BuildVersion = buildVers;
             Name = name;
+            RecordedDate = dateTime;
         }
         public bool Contains(Transaction t)
         {
