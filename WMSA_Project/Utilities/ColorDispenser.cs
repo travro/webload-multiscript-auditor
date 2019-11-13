49be8fcda6497cb3d241586a6c91715ba21f6778
tsmelvin <travis.melvin@sumtotalsystems.com>
2019-11-13 16:12:20 -0500

    ADD utility classes
    new ColorDispenser and StackPanelFactory singletons

---
+++ WMSA_Project/Utilities/ColorDispenser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMSA_Project.Utilities
+{
+    public sealed class ColorDispenser
+    {
+        private Queue<SolidColorBrush> _brushes;
+        private static ColorDispenser _colorDispenser;
+
+        private ColorDispenser()
+        {
+            _brushes = new Queue<SolidColorBrush>(
+                new List<SolidColorBrush>
+                {
+                  //  Brushes.AliceBlue,
+                  //  Brushes.AntiqueWhite,
+                    Brushes.Aqua,
+                   // Brushes.Aquamarine,
+                  //  Brushes.Azure,
+                    Brushes.Beige,
+                  //  Brushes.Bisque,
+                  //  Brushes.Blue,
+                   // Brushes.BurlyWood,
+                    Brushes.GreenYellow
+                });
+        }
+        public static ColorDispenser Dispenser
+        {
+            get
+            {
+                if (_colorDispenser == null)
+                {
+                    _colorDispenser = new ColorDispenser();
+                }
+                return _colorDispenser;
+            }
+        }
+
+        public SolidColorBrush GetNextColor()
+        {
+            var brush = _brushes.Dequeue();
+
+            if (brush != null)
+            {
+                _brushes.Enqueue(brush);
+                return brush;
+            }
+            else return Brushes.Red;
+        }
+        
+    }
+}
---
+++ WMSA_Project/Utilities/Factories/StackPanelFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMSA_Project.Models.Repositories;
+
+namespace WMSA_Project.Utilities.Factories
+{
+    public static class StackPanelFactory
+    {
+        public static void ProvideStackPanels(ScriptRepository repo)
+        {
+
+        }
+    }
+}
