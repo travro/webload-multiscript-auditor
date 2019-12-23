78ee470667c0a6b7151703cca6066e54478f08c8
tsmelvin <travis.melvin@sumtotalsystems.com>
2019-12-23 14:28:41 -0500

    ADD UnmatchedRequeset class

---
+++ WMSA_Project/Models/UnmatchedRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMSA_Project.Models
+{
+    public class UnmatchedRequest : Request
+    {
+        public Script UnmatchedSource { get; set; }
+
+        public UnmatchedRequest(Script s):base()
+        {
+            UnmatchedSource = s;
+        }
+        public UnmatchedRequest(Script s, RequestVerb verb, string parameters, bool visible): base(verb, parameters, visible)
+        {
+            UnmatchedSource = s;
+        }
+    }
+}
