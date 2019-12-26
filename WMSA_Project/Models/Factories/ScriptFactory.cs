d79da0d49484c901b2e38a8a26088823d1d97399
tsmelvin <travis.melvin@sumtotalsystems.com>
2019-12-26 14:06:29 -0500

    UPDATE Request.cs
    re added Matched property

--- WMSA_Project/Models/Factories/ScriptFactory.cs
+++ WMSA_Project/Models/Factories/ScriptFactory.cs
@@ -8,6 +8,7 @@ using System.Xml;
 using System.Xml.Linq;
 using WMSA_Project.Models;
 using WMSA_Project.Controls;
+using WMSA_Project.Utilities;
 
 namespace WMSA_Project.Models.Factories
 {
@@ -27,7 +28,7 @@ namespace WMSA_Project.Models.Factories
                     using (XmlReader _xReader = XmlReader.Create(_fStream))
                     {
                         XDocument _XDoc = XDocument.Load(_xReader);
-                        script.Transactions = TransactionListFactory.GetTransactionsFromXDoc(_XDoc);
+                        script.Transactions = TransactionListFactory.GetTransactionsFromXDoc(_XDoc, script);
                     }
                 }
             }
@@ -40,25 +41,22 @@ namespace WMSA_Project.Models.Factories
 
         public static Script GetComparativeScriptFromControls(ScriptControl baseControl, ScriptControl leftControl = null, ScriptControl rightControl = null)
         {
-            return baseControl.Script.Clone();
-            
-            //var cloneScript = baseControl.Script.Clone();
-            
-            /*baseControl.Script.Transactions.ForEach((t) =>
+            baseControl.Script.ClearUnmatchedRequests();
+            baseControl.Script.Transactions.ForEach((t) =>
             {
                 if (leftControl != null)
                 {
-                    var otherT = leftControl.Script.Transactions.First((leftT) => leftT.Name == t.Name);
-                    //SomeStaticClass.InsertComparativeData(t, oT);
-                }
-                
-                if(rightControl != null)
-                {
-                    var otherT = rightControl.Script.Transactions.First((rightT) => rightT.Name == t.Name);
-                    //SomeStaticClass.InsertComparativeData(t, oT);
+                    var leftControlTransaction = leftControl.Script.Transactions.First((leftT) => leftT.Name == t.Name);
+                    ScriptTransactionsComparer.MatchRequests(t, leftControlTransaction, leftControl.LabelColor);
                 }
 
-            });*/
+                if (rightControl != null)
+                {
+                    var rightControlTransaction = rightControl.Script.Transactions.First((rightT) => rightT.Name == t.Name);
+                    ScriptTransactionsComparer.MatchRequests(t, rightControlTransaction, rightControl.LabelColor);
+                }
+            });
+            return baseControl.Script;
         }
     }
 }
