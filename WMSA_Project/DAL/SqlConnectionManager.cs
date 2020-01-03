f748c6f8487e096d4da2b3d0f22ded2a3c47c78d
tsmelvin <travis.melvin@sumtotalsystems.com>
2020-01-03 12:30:55 -0500

    UPDATE SqlConnectionManager.cs

--- WMSA_Project/DAL/SqlConnectionManager.cs
+++ WMSA_Project/DAL/SqlConnectionManager.cs
@@ -1,14 +1,39 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WMSA_Project.DAL
 {
     public static class SqlConnectionManager
     {
+        private static SqlConnection GetOpenMasterConnection()
+        {
+            var sqlConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["master"].ConnectionString);
+            sqlConnection.Open();
+            return sqlConnection;
+        }
         public static SqlConnection GetOpenConnection()
         {
             var sqlConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["WLScriptsDB"].ConnectionString);
             sqlConnection.Open();
             return sqlConnection;
+        }
+        public static bool CheckDatabaseAvailability()
+        {
+            try
+            {
+                var cnn = GetOpenConnection();
+                if (cnn != null)
+                {
+                    cnn.Close();
+                    return true;
+                }
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
