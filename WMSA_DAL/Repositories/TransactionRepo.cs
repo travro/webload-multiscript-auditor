1ee869918234c7ea2379f27aa91c2336e6589f5d
tsmelvin <travis.melvin@sumtotalsystems.com>
2020-01-22 14:55:01 -0500

    ADD TransactionRepo.cs and ScriptFactory.cs
    Added new repo and service namespace to DAL

---
+++ WMSA_DAL/Repositories/TransactionRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMSA_DAL.Models;
+using WMSA_DAL.Interfaces;
+
+namespace WMSA_DAL.Repositories
+{
+    public class TransactionRepo: WLObjectRepo<Transaction>, IRepo<Transaction>
+    {
+        public TransactionRepo()
+        {
+            _table = Context.Transactions;
+        }
+
+        public int Delete(int entityId)
+        {
+            Context.Entry(new Transaction() { Id = entityId }).State = System.Data.Entity.EntityState.Deleted;
+            return SaveChanges();
+        }
+
+        public Task<int> DeleteAsync(int entityId)
+        {
+            Context.Entry(new Transaction() { Id = entityId }).State = System.Data.Entity.EntityState.Deleted;
+            return SaveChangesAsync();
+        }
+    }
+}
---
+++ WMSA_DAL/Service/ScriptFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMSA_DAL.Service
+{
+    public class ScriptFactory
+    {
+        public static void SayHi()
+        {
+
+        }
+    }
+}
