4759a245bb2cf0084b528a3e141507b031e5c349
tsmelvin <travis.melvin@sumtotalsystems.com>
2020-08-31 15:26:54 -0400

    FIX URL parsing
    patched url strings not parsing community values

--- WMSA_Project/Models/Factories/RequestListFactory.cs
+++ WMSA_Project/Models/Factories/RequestListFactory.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
 using WMSA_Project.Models;
@@ -11,6 +13,16 @@ namespace WMSA_Project.Models.Factories
 {
     internal static class RequestListFactory
     {
+        private static readonly string[][] _boundRules = new string[][]
+        {
+            new[]{ "[TenantKey]", "/learning/DataStore/", "/" },
+            new[]{ "[TimeStamp]", "blank___", "."},
+            new[]{ "[CommunityId]", "/communities/", "/"},
+            new[]{ "[TenantKey]", "contentengine/TCAPI/", "/"},
+            new[]{ "[UserId]", "gamification/summaries/", null},
+            new[]{ "[CommunityId]", "/communities/", null},
+            new[]{ "[DiscussionId]", "api/social/discuss", null},
+        };
         public static List<Request> GetRequestsFromXElement(XElement xElement, bool addNonVisibles = true)
         {
             var requests = new List<Request>();
@@ -32,7 +44,7 @@ namespace WMSA_Project.Models.Factories
 
                 //the first request value in the string is the only visible value in the Webload IDE
                 foreach (var httpEl in httpHeaderElements)
-                { 
+                {
                     if (httpEl == httpHeaderElements.First() && nodeScriptEl.Value.Contains("setCorr"))
                     {
                         requests.Add(new Request()
@@ -61,7 +73,7 @@ namespace WMSA_Project.Models.Factories
                                 Verb = ParseRequestVerb(httpEl),
                                 URL = ParseRequestUrl(httpEl),
                                 Visible = false
-                            }); 
+                            });
                         }
                     }
                 }
@@ -88,62 +100,42 @@ namespace WMSA_Project.Models.Factories
             string domain = (line.Contains(sumTotalSite)) ? sumTotalSite : "https://";
             int domainLastIndex = line.IndexOf(domain) + domain.Length;
             int paramIndex = line.IndexOfAny(new[] { '?', '%', ' ' }, domainLastIndex);
-
             string filteredUrl = line.Substring(domainLastIndex, paramIndex - domainLastIndex);
 
-            filteredUrl = filteredUrl.ReplaceInBounds("[TenantKey]", "/learning/DataStore/", "/Learning/");
-            filteredUrl = filteredUrl.ReplaceInBounds("[TenantKey]", "/learning/DataStore/", "/Common/");
-            filteredUrl = filteredUrl.ReplaceInBounds("[TimeStamp]", "blank___", ".");
-            filteredUrl = filteredUrl.ReplaceInBounds("[UserId]", "gamification/summaries/");
-            filteredUrl = filteredUrl.ReplaceInBounds("[CommunityId]", "api/sumtSocial/communities/");
-            filteredUrl = filteredUrl.ReplaceInBounds("[CommunityId]", "api/sumtSocial/communities/", "/");
-            filteredUrl = filteredUrl.ReplaceInBounds("[CommunityId]", "/communities/", "/blogs");
-            filteredUrl = filteredUrl.ReplaceInBounds("[DiscussionId]", "api/social/discuss");
-            filteredUrl = filteredUrl.ReplaceInBounds("[TenantKey]", "contentengine/TCAPI/", "/");
+            foreach (string[] rule in _boundRules)
+            {
+                if (!filteredUrl.Contains(rule[1])) continue;
+                if (rule[2] != null && !filteredUrl.Substring(filteredUrl.IndexOf(rule[1]) + rule[1].Length).Contains(rule[2])) continue;
+
+                int lBIndex = filteredUrl.IndexOf(rule[1]) + rule[1].Length;
+
+                if (lBIndex >= filteredUrl.Length - 1) break;
+
+                int rBIndex; 
+                string originalValue;
+
+                if(rule[2] != null)
+                {
+                    rBIndex = filteredUrl.IndexOf(rule[2], lBIndex);
+                    originalValue = filteredUrl.Substring(lBIndex, rBIndex - lBIndex);
+                }
+                else
+                {
+                    originalValue = filteredUrl.Substring(lBIndex);
+                }
+                var strBuilder = new StringBuilder(filteredUrl);
+
+                strBuilder.Replace(originalValue, rule[0]);
+                filteredUrl = strBuilder.ToString();
+                break;
+            }
 
             //truncate ending front slash
             if (filteredUrl.LastIndexOf('/') == filteredUrl.Length - 1)
             {
                 filteredUrl = filteredUrl.Remove(filteredUrl.Length - 1);
             }
-
             return filteredUrl;
         }
     }
-    //Custom extension methods for checking for different boundaries and replacing internal values if checks are truthy 
-    internal static class StringExtension
-    {
-        public static string ReplaceInBounds(this string url, string replacementVal, string leftBound, string rightBound = null)
-        {
-            if (!url.Contains(leftBound)) return url;
-            if (rightBound != null && !url.Substring(url.IndexOf(leftBound) + leftBound.Length).Contains(rightBound)) return url;
-
-
-            int leftBoundIndex = url.IndexOf(leftBound) + leftBound.Length;
-
-            if (leftBoundIndex >= url.Length - 1) return url;
-
-            int rightBoundIndex;
-            string originalValue;
-
-            if (rightBound != null)
-            {
-                rightBoundIndex = url.IndexOf(rightBound, leftBoundIndex);
-                originalValue = url.Substring(leftBoundIndex, rightBoundIndex - leftBoundIndex);
-            }
-            else
-            {
-                originalValue = url.Substring(leftBoundIndex);
-            }
-
-            var strBuilder = new StringBuilder(url);
-            strBuilder.Replace(originalValue, replacementVal);
-
-            
-
-            url = strBuilder.ToString();
-            return url;
-        }
-    }
 }
-
