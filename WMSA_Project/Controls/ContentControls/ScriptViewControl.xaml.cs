11813466451df755db3178d08187577cf7bbac4b
tsmelvin <travis.melvin@sumtotalsystems.com>
2020-09-02 12:00:11 -0400

    ADD Content control classes
    main window now uses content loader

---
+++ WMSA_Project/Controls/ContentControls/ScriptViewControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMSA_Project.Repositories;
+
+namespace WMSA_Project.Controls.ContentControls
+{
+    /// <summary>
+    /// Interaction logic for ScriptViewControl.xaml
+    /// </summary>
+    public partial class ScriptViewControl : UserControl
+    {
+        public ScriptViewControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            foreach (var scc in ScriptCollectionContainer.ThisContainer.List)
+            {
+                StkPnl_Main.Children.Add(scc);
+            }
+
+            if (ScriptCollectionContainer.ThisContainer != null)
+            {
+                ScriptCollectionContainer.ThisContainer.CollectionChanged += UpdateList;
+            }
+        }
+
+        private void UpdateList(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            StkPnl_Main.Children.Clear();
+
+            foreach (var scc in ScriptCollectionContainer.ThisContainer.List)
+            {
+                StkPnl_Main.Children.Add(scc);
+            }
+        }
+    }
+}
---
+++ WMSA_Project/Utilities/ContentLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMSA_Project.Controls.ContentControls;
+
+namespace WMSA_Project.Utilities
+{
+    public sealed class ContentLoader
+    {
+        private ScriptViewControl _scriptViewControl;
+        private SutViewControl _sutViewControl;
+        private TableViewControl _tableViewControl;
+
+        public ContentLoader() { }
+
+        public UserControl LoadScriptView()
+        {
+            if (_scriptViewControl == null) { _scriptViewControl = new ScriptViewControl(); }
+            return _scriptViewControl;
+        }
+
+        public UserControl LoadTableView()
+        {
+            if (_tableViewControl == null) { _tableViewControl = new TableViewControl(); }
+            return _tableViewControl ?? new TableViewControl();
+        }
+
+        public UserControl LoadSUTView()
+        {
+            if (_sutViewControl == null) { _sutViewControl = new SutViewControl(); }
+            return _sutViewControl ?? new SutViewControl();
+        }
+
+    }
+}
