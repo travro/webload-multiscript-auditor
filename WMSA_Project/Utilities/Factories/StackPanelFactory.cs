89332bb720e0cea698f2b33e3262963ee6c3033a
tsmelvin <travis.melvin@sumtotalsystems.com>
2020-09-03 23:08:06 -0400

    ADD ListView handler

--- WMSA_Project/Utilities/Factories/StackPanelFactory.cs
+++ WMSA_Project/Utilities/Factories/StackPanelFactory.cs
@@ -49,8 +49,10 @@ namespace WMSA_Project.Utilities.Factories
             {
                 var transExpander = new Expander() { Header = new StackPanel() { Orientation = Orientation.Horizontal } };
                 transExpander.DataContext = transExpander;
-                //transExpander.Content
+
                 var reqListView = new ListView();
+
+                //Set MouseWheel event to parent
                 reqListView.PreviewMouseWheel += (object sender, MouseWheelEventArgs args) =>
                 {
                     if (!args.Handled)
@@ -64,110 +66,60 @@ namespace WMSA_Project.Utilities.Factories
                     }
                 };
 
-                var reqGridView = new GridView();
+                //Format GridView
+                var reqGridView = new GridView()
+                {
+
+                };
                 reqGridView.Columns.Add(new GridViewColumn() { Header = "Method" });
                 reqGridView.Columns.Add(new GridViewColumn() { Header = "URL" });
                 reqGridView.Columns.Add(new GridViewColumn() { Header = "Corrs" });
                 reqGridView.Columns[0].DisplayMemberBinding = new Binding("Verb");
                 reqGridView.Columns[1].DisplayMemberBinding = new Binding("URL");
                 reqGridView.Columns[2].DisplayMemberBinding = new Binding("Correlations.Count");
-                reqListView.View = reqGridView;                
+                reqListView.View = reqGridView;
 
+                //Add requests to Listview
                 if (t.Requests != null)
                 {
+                    ListViewItem lstViewItem;
+
                     foreach (var r in t.Requests)
                     {
-                        //if (r.Correlations != null && r.Correlations.Count > 0)
-                        //{
-                        //    reqGridView.Columns[2].DisplayMemberBinding = new Binding("Correlations.Count");
-                        //}
+                        lstViewItem = new ListViewItem()
+                        {
+                            Content = r,
+                            
+                        };
 
+                        //assign selection handler
+                        lstViewItem.Selected += (object sender, RoutedEventArgs args) => { MessageBox.Show($"Message box clicked, {r.GetInfoString()}"); };
 
-                        //var reqTxtBlk = new TextBlock();
-                        //reqTxtBlk.Text = r.GetInfoString();    
-                        //reqTxtBlk.DataContext = reqTxtBlk;
-
-                        //if (!isFirst && !r.Matched)
-                        //{
-                        //    reqTxtBlk.Foreground = Brushes.Green;
-                        //    //reqTxtBlk.Background = Brushes.WhiteSmoke;
-                        //    reqTxtBlk.Text = $"+ {reqTxtBlk.Text}";
-                        //}
-
-                        //if (r.Correlations != null && r.Correlations.Count > 0)
-                        //{
-                        //    reqTxtBlk.Text = $"{reqTxtBlk.Text} | {r.Correlations.Count} correlations";
-                        //}
-                        reqListView.Items.Add(r);
+                        if (!isFirst && !r.Matched)
+                        {
+                            lstViewItem.Foreground = Brushes.Green;
+                        }
+                        reqListView.Items.Add(lstViewItem);
                     }
 
                     foreach (var dR in t.RequestsDropped)
                     {
-                        //var reqTxtBlk = new TextBlock();
-                        //reqTxtBlk.Text = dR.GetInfoString();
-                        //reqTxtBlk.DataContext = reqTxtBlk;
-                        //reqTxtBlk.Foreground = Brushes.Red;
-                        //reqTxtBlk.TextDecorations = TextDecorations.Strikethrough;
-                        reqListView.Items.Add(dR);
+
+                        dR.URL += " [DROPPED]";
+
+                        lstViewItem = new ListViewItem()
+                        {
+                            Content = dR,
+                            Foreground = Brushes.Red,
+                            IsEnabled = false
+                        };
+
+                        reqListView.Items.Add(lstViewItem);
                     }
                 }
                 transExpander.Content = reqListView;
 
-                /**
-                //var reqTree = new TreeView();
-
-                //if (t.Requests != null)
-                //{
-                //    foreach (var r in t.Requests)
-                //    {
-                //        var reqTreeViewItem = new TreeViewItem()
-                //        {
-                //            Header = r.GetInfoString(),
-                //        };
-
-                //        reqTreeViewItem.DataContext = reqTreeViewItem;
-
-                //        if (!isFirst && !r.Matched)
-                //        {
-                //            reqTreeViewItem.Foreground = Brushes.Green;
-                //            reqTreeViewItem.Background = Brushes.WhiteSmoke;
-                //            reqTreeViewItem.Header = "+" + reqTreeViewItem.Header;
-                //        }
-
-                //        if (r.Correlations != null)
-                //        {
-                //            foreach (var c in r.Correlations)
-                //            {
-                //                var corrTreeViewItem = new TreeViewItem()
-                //                {
-                //                    Header = c.GetInfoString(),
-                //                };
-                //                corrTreeViewItem.DataContext = corrTreeViewItem;
-                //                reqTreeViewItem.Items.Add(corrTreeViewItem);
-                //            }
-                //        }
-                //        reqTree.Items.Add(reqTreeViewItem);
-                //    }
-                //}
-
-                //if (t.RequestsDropped != null && t.RequestsDropped.Count > 0)
-                //{
-                //    foreach (var unReq in t.RequestsDropped)
-                //    {
-                //        var unReqTreeViewItem = new TreeViewItem()
-                //        {
-                //            Header = "-" + unReq.GetInfoString(),
-                //            Foreground = Brushes.Red,
-                //            Background = Brushes.WhiteSmoke
-                //        };
-                //        unReqTreeViewItem.DataContext = unReqTreeViewItem;
-                //        reqTree.Items.Add(unReqTreeViewItem);
-                //    }
-                //}
-
-                //transExpander.Content = reqListView;
-                */
-
+                //Format Transaction Expander Header
                 var transExpanderHeader = (transExpander.Header as StackPanel);
                 transExpanderHeader.Children.Add(new TextBlock() { Text = t.Name });
 
@@ -195,6 +147,8 @@ namespace WMSA_Project.Utilities.Factories
                         Foreground = Brushes.Red
                     });
                 }
+
+                //Add Transaction to script control stack panel
                 scriptControl.Stack_Transactions.Children.Add(transExpander);
             }
         }
